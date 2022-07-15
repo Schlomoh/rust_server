c7041c66e486721ffc89ca0be645db374d362a70
Schlomoh <post@moritzbecker.de>
2022-07-15 14:46:41 +0200

    add requests

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ mod utils;
 use std::thread::sleep;
 use std::time::Duration;
 // local modules
-use networking::requests::{RequestSetup};
+use networking::requests::RequestSetup;
+use utils::arg_handling::parse_args;
 use utils::logging::Logger;
 use utils::timing::Timing;
-use utils::arg_handling::parse_args;
 
 fn main() {
     let args = parse_args();
@@ -16,22 +16,15 @@ fn main() {
         mode: args.verbosity,
     };
 
-    let r = RequestSetup {
-        host: "bla".to_string(),
-        base_file_url: "bla".to_string(),
-    };
-
-    let mut t = Timing {
+    let t = Timing {
         start: None,
         duration: None,
     };
 
-    t.start_timer();
-    let sleeping_time = Duration::from_millis(1234);
-    sleep(sleeping_time);
-    t.end_timer();
-    println!("{:?}", t.duration);
-    println!("{:?}", t.get_timestamp());
-    println!("{:?}", t.convert_to_milliseconds());
-    // println!("{}", t.get_timestamp());
+    let r = RequestSetup {
+        host: "bla".to_string(),
+        test_file_url: "bla".to_string(),
+        timer: t,
+        download_iteration: 50,
+    };
 }
---
+++ src/networking/network_tests.rs
@@ -0,0 +1,24 @@
+pub struct Network_testing {
+    test_download_speed: bool,
+
+}
+
+impl Network_testing {
+
+    let r = RequestSetup {
+        host: "bla".to_string(),
+        test_file_url: "bla".to_string(),
+        timer: t,
+        download_iteration: 50,
+    };
+
+    fn test_download_speed(&self) {
+
+    }
+    
+    pub fn start(&self) {
+        if self.test_download_speed {
+
+        }
+    }fads
+}
